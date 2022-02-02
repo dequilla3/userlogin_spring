8180528d79b9dc0963abe8b3d47ce37329ecf01d
Kim Dequilla <67781364+dequilla3@users.noreply.github.com>
2022-02-02 20:15:40 +0800

    added email sender

---
+++ src/main/java/com/example/userlogin/service/EmailService.java
@@ -0,0 +1,38 @@
+package com.example.userlogin.service;
+
+import com.example.userlogin.core.EmailSender;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+@AllArgsConstructor
+public class EmailService implements EmailSender {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(EmailSender.class);
+    private final JavaMailSender javaMailSender;
+
+    @Override
+    @Async
+    public void send(String to, String email) {
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setFrom("deqkim4@gmail.com");
+            javaMailSender.send(mimeMessage);
+
+        } catch (MessagingException ex) {
+            LOGGER.error("Failed to send email", ex);
+            throw new RuntimeException("Failed to send email");
+        }
+    }
+}
